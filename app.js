0764c0fdb7b724b4d62473f9fcd628dbc00f98e2
martinolega <martinolega11@gmail.com>
2026-01-12 23:44:58 +0100

    feat:middlewares

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 import express from "express";
 import postRouter from "./routers/posts.js";
 import postList from "./data/postList.js";
+import routeNotFound from "./middlewares/routeNotFound.js";
+import error from "./middlewares/error.js";
 
 const app = express();
-app.use(express.json());
 const port = 3000;
 
+app.use(express.json());
 app.use(express.static("public"));
-app.use("/posts", postRouter);
-
-app.listen(port, () => {
-    console.log("Aperto l'host locale su porta " + port);
-});
 
 app.get("/", (req, res) => {
     res.send("Server del mio blog");
@@ -19,4 +16,14 @@ app.get("/", (req, res) => {
 
 app.get("/bacheca", (req, res) => {
     res.json(postList);    
+});
+
+app.use("/posts", postRouter);
+
+app.use(routeNotFound);
+
+app.use(error);
+
+app.listen(port, () => {
+    console.log("Aperto l'host locale su porta " + port);
 });
